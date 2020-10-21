b94e6fecc5ede595e6071bdd5eeb1dfd918c5813
Marcus Kringberg <marcus.kringberg@gmail.com>
2020-10-21 11:06:42 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Detta ar ett test av Ghub");
+
+	// write your code here
+    }
+}
